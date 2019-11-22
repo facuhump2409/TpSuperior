aaafe562337180b367fcfeb2a428bf142fdfa9e6
Rodrigo <Rodrigo@DESKTOP-SKN9E15.fibertel.com.ar>
2019-11-22 18:23:05 -0300

    asd

--- src/main/java/tpSuperior/MainPage.java
+++ src/main/java/tpSuperior/MainPage.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.ScrollPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -24,8 +25,6 @@ import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
-import javax.swing.JTextField;
-import java.awt.ScrollPane;
 
 public class MainPage {
 
